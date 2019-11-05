931aaedcab5d5291d4c72bd909ee7ec0488728c8
Juan Ignacio Tarallo <juanignaciotarallo@gmail.com>
2019-11-05 20:16:24 -0300

    Networking y decoding de JSON terminado

--- LibreriaFlux/ContentView.swift
+++ LibreriaFlux/ContentView.swift
@@ -8,7 +8,41 @@
 
 import SwiftUI
 
+let apiUrl = "https://qodyhvpf8b.execute-api.us-east-1.amazonaws.com/test/books"
+
+struct Book: Identifiable, Decodable {
+    let id: Int
+    let nombre: String
+    let autor: String
+    let disponibilidad: Bool
+    let popularidad: Int
+    let imagen: String
+}
+
+class BooksViewModel: ObservableObject {
+    @Published var books: [Book] = [
+        .init(id: 1, nombre: "Libro 1", autor: "Autor 1", disponibilidad: true, popularidad: 100, imagen: "https://www.google.com.ar"),
+        .init(id: 2, nombre: "Libro 2", autor: "Autor 2", disponibilidad: false, popularidad: 80, imagen: "https://www.google.com.ar"),
+        .init(id: 3, nombre: "Libro 3", autor: "Autor 3", disponibilidad: true, popularidad: 60, imagen: "https://www.google.com.ar")
+    ]
+    
+    func fetchBooks() {
+        guard let url = URL(string: apiUrl) else { return }
+        URLSession.shared.dataTask(with: url) { (data, resp, err) in
+            DispatchQueue.main.async {
+                do {
+                    self.books = try JSONDecoder().decode([Book].self, from: data!)
+                } catch {
+                    print("Failed to decode JSON: ", error)
+                }
+            }
+        }.resume()
+    }
+}
+
 struct ContentView: View {
+    
+    @ObservedObject var booksVM = BooksViewModel()
     var body: some View {
         Text("Hello, World!")
     }
