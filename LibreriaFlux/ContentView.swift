8d689cf8e4c7b427a64db174f6f70ad0c4118194
Juan Ignacio Tarallo <juanignaciotarallo@gmail.com>
2019-11-05 19:38:22 -0300

    Initial Commit

---
+++ LibreriaFlux/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LibreriaFlux
+//
+//  Created by Juan Ignacio Tarallo on 05/11/2019.
+//  Copyright © 2019 Juan Ignacio Tarallo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
