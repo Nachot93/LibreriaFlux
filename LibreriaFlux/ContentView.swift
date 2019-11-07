d10c93d9b3ac924c9cf6644c2b8a1cc89517c822
Juan Ignacio Tarallo <juanignaciotarallo@gmail.com>
2019-11-07 14:24:07 -0300

    Funcionalidad basica lista, imagenes WIP

--- LibreriaFlux/ContentView.swift
+++ LibreriaFlux/ContentView.swift
@@ -16,15 +16,52 @@ struct Book: Identifiable, Decodable {
     let autor: String
     let disponibilidad: Bool
     let popularidad: Int
-    let imagen: String
+    let imagen: String = ""
+}
+
+struct ImageView: View {
+    @ObservedObject var imageLoader:ImageLoader
+    @State var image:UIImage = UIImage()
+
+    func imageFromData(_ data:Data) -> UIImage {
+        UIImage(data: data) ?? UIImage()
+    }
+
+    init(withURL url:String) {
+        imageLoader = ImageLoader(urlString:url)
+    }
+
+    var body: some View {
+        VStack {
+
+            Image(uiImage: imageLoader.data != nil ? UIImage(data:imageLoader.data!)! : UIImage())
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width:100, height:100)
+        }
+    }
+
+}
+
+class ImageLoader: ObservableObject {
+    @Published var dataIsValid = false
+    var data:Data?
+
+    init(urlString:String) {
+        guard let url = URL(string: urlString) else { return }
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data else { return }
+            DispatchQueue.main.async {
+                self.dataIsValid = true
+                self.data = data
+            }
+        }
+        task.resume()
+    }
 }
 
 class BooksViewModel: ObservableObject {
-    @Published var books: [Book] = [
-        .init(id: 1, nombre: "Libro 1", autor: "Autor 1", disponibilidad: true, popularidad: 100, imagen: "https://www.google.com.ar"),
-        .init(id: 2, nombre: "Libro 2", autor: "Autor 2", disponibilidad: false, popularidad: 80, imagen: "https://www.google.com.ar"),
-        .init(id: 3, nombre: "Libro 3", autor: "Autor 3", disponibilidad: true, popularidad: 60, imagen: "https://www.google.com.ar")
-    ]
+    @Published var books: [Book] = []
     
     func fetchBooks() {
         guard let url = URL(string: apiUrl) else { return }
@@ -43,8 +80,35 @@ class BooksViewModel: ObservableObject {
 struct ContentView: View {
     
     @ObservedObject var booksVM = BooksViewModel()
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            List {
+                VStack(alignment: .leading) {
+                    ForEach(booksVM.books.sorted { $0.popularidad > $1.popularidad}) { book in
+                        HStack {
+                            ImageView(withURL: book.imagen)
+                            Text(book.nombre)
+                            Spacer()
+                            Text(String(book.popularidad))
+                        }
+                        HStack {
+                            Text(book.autor)
+                            Spacer()
+                            if book.disponibilidad {
+                                Text("Disponible")
+                            } else {
+                                Text("No disponible")
+                            }
+                        }
+                        Spacer()
+                    }
+                }.padding([.leading, .trailing], 5)
+            }
+            .navigationBarTitle("Bienvenido a la librería flux")
+            .onAppear(perform: self.booksVM.fetchBooks)
+        }
+        
     }
 }
 
